a9b9f526b0bbecfe6be40def393fefb3975e5669
DonnyDianderas <donnydianderas@gmail.com>
2026-03-30 15:30:11 -0500

    make product creation dynamic using auth session ID

--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -1,9 +1,20 @@
 import CreateProductForm from '@/app/ui/products/create-form';
 import { playfair } from '@/app/ui/fonts';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
-export default function Page() {
+export default async function Page() {
   // Temporary ID for Alfredo Naucapoma (woodworker) 
-  const tempArtisanId = "5f2efbdd-5bbb-4138-b167-b84938c937d7"; 
+  //const tempArtisanId = "5f2efbdd-5bbb-4138-b167-b84938c937d7"; 
+
+  // Get the current session to identify the logged-in artisan
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  // If  there is no user, we shouldn't show the form
+  if (!userId) {
+    redirect('/login');
+  }
 
   return (
     <div style={{ padding: '40px 0' }}>
@@ -16,8 +27,8 @@ export default function Page() {
         Add New Product to Shop
       </h1>
       
-      {/* Rendering the form component with the temporary artisan ID */}
-      <CreateProductForm artisanId={tempArtisanId} />
+      
+      <CreateProductForm artisanId={userId} />
 
     </div>
   );
