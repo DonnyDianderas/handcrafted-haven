2d42422ef39b66efa3ae68e4065aa637faa28d6b
Brantley Cragun <bccragun@yahoo.com>
2026-04-02 20:08:01 -0600

    Added Price Filter

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -4,13 +4,29 @@ import Link from "next/link";
 import { sql } from '@vercel/postgres'; 
 import { playfair } from "@/app/ui/fonts";
 
-export default async function CatalogPage() {
-  const { rows: products } = await sql`SELECT * FROM products`;
+export default async function CatalogPage({ searchParams, }: {searchParams: {sort?: string};}) {
+  const params = await searchParams;
+  const sort = params.sort === "desc" ? "desc" : "asc";
+
+  const { rows: products } = 
+    sort === "desc"
+      ? await sql`SELECT * FROM products ORDER BY price DESC`
+      : await sql`SELECT * FROM products ORDER BY price ASC`;
 
   return (
     <section className={styles.container}>
       <h1 className={`${styles.title} ${playfair.className}`}>Our Collection</h1>
-      
+
+      <form method="get" className={styles.filterForm}>
+        <label htmlFor="sort">Sort Products:</label>
+        <select id="sort" name="sort" defaultValue={sort}>
+          <option value="asc">Low to High</option>
+          <option value="desc">High to Low</option>
+        </select>
+
+        <button type="submit">Sort</button>
+      </form>
+
       <div className={styles.grid}>
         {products.map((product) => (
           <div key={product.id} className={styles.card}>
