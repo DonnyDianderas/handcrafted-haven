36771682be9952ad0fa37bc69e4d7097bab1b962
jcbcnielsen <jcbcnielsen@proton.me>
2026-04-12 22:43:29 -0400

    Ability to see reviews on product detail pages added

--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { notFound } from 'next/navigation';
-import { fetchProductById } from '@/app/lib/data';
+import { fetchProductById, fetchReviewsByProductId } from '@/app/lib/data';
 import ProductDetail from '@/app/ui/products/ProductDetail';
+import ProductReview from "@/app/ui/products/ProductReview";
+import styles from "./product-review-section.module.css";
 
 interface PageProps {
   params: Promise<{ id: string }>;
@@ -14,16 +16,42 @@ export default async function ProductDetailPage({ params }: PageProps) {
     notFound();
   }
 
+  const reviews = await fetchReviewsByProductId(id);
+  let ReviewSection = null;
+  if (reviews && reviews.length) {
+    ReviewSection = reviews.map(async function (review) {
+      return (
+        <ProductReview
+          rating={review.rating}
+          reviewText={review.review_text}
+          customerName={review.name}
+        />
+      );
+    });
+  } else {
+    ReviewSection = (
+      <p>There don't seem to be any reviews!</p>
+    );
+  }
+
   return (
-    <ProductDetail
-      id={product.id}
-      name={product.name}
-      price={product.price}
-      image_url={product.image_url}
-      artisan_id={product.artisan_id}
-      artisan_name={product.artisan_name}
-      category={product.category}
-      description={product.description}
-    />
+    <>
+      <ProductDetail
+        id={product.id}
+        name={product.name}
+        price={product.price}
+        image_url={product.image_url}
+        artisan_id={product.artisan_id}
+        artisan_name={product.artisan_name}
+        category={product.category}
+        description={product.description}
+      />
+      <section className={styles.reviewContainer}>
+        <h2>Reviews</h2>
+        <section className={styles.reviewGrid}>
+          {ReviewSection}
+        </section>
+      </section>
+    </>
   );
 }
